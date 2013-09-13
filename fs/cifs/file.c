29edbbbe90cd08fc644eca13211be2277bc39ac9
Sachin Prabhu <sprabhu@redhat.com>
2013-09-13 14:11:56 +0100

    cifs: Do not take a reference to the page in cifs_readpage_worker()
    We do not need to take a reference to the pagecache in
    cifs_readpage_worker() since the calling function will have already
    taken one before passing the pointer to the page as an argument to the
    function.
    
    Signed-off-by: Sachin Prabhu <sprabhu@redhat.com>
    Reviewed-by: Jeff Layton <jlayton@redhat.com>
    Signed-off-by: Steve French <smfrench@gmail.com>

--- fs/cifs/file.c
+++ fs/cifs/file.c
@@ -3410,6 +3410,9 @@ static int cifs_readpages(struct file *file, struct address_space *mapping,
 	return rc;
 }
 
+/*
+ * cifs_readpage_worker must be called with the page pinned
+ */
 static int cifs_readpage_worker(struct file *file, struct page *page,
 	loff_t *poffset)
 {
@@ -3421,7 +3424,6 @@ static int cifs_readpage_worker(struct file *file, struct page *page,
 	if (rc == 0)
 		goto read_complete;
 
-	page_cache_get(page);
 	read_data = kmap(page);
 	/* for reads over a certain size could initiate async read ahead */
 
@@ -3448,7 +3450,6 @@ static int cifs_readpage_worker(struct file *file, struct page *page,
 
 io_error:
 	kunmap(page);
-	page_cache_release(page);
 
 read_complete:
 	return rc;
